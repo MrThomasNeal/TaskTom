fb5d00c6246feeb1551d0128325a8bd6537f62cf
Thomas Neal <psytg3@nottingham.ac.uk>
2024-06-21 15:26:30 +0100

    Welcome functionality implemented

--- MyBot/MyBot.cpp
+++ MyBot/MyBot.cpp
@@ -1,26 +1,19 @@
 #include "MyBot.h"
 #include <dpp/dpp.h>
 
-/* Be sure to place your token in the line below.
- * Follow steps here to get a token:
- * https://dpp.dev/creating-a-bot-application.html
- * When you invite the bot, be sure to invite it with the 
- * scopes 'bot' and 'applications.commands', e.g.
- * https://discord.com/oauth2/authorize?client_id=940762342495518720&scope=bot+applications.commands&permissions=139586816064
- */
-const std::string    BOT_TOKEN    = "add your token here";
+const std::string    BOT_TOKEN    = "";
 
 int main()
 {
-	/* Create bot cluster */
-	dpp::cluster bot(BOT_TOKEN);
+	// Create bot cluster
+	dpp::cluster bot(BOT_TOKEN, dpp::i_default_intents | dpp::i_guild_members | dpp::i_message_content);
 
-	/* Output simple log messages to stdout */
+	// Output simple log messages to stdout
 	bot.on_log(dpp::utility::cout_logger());
 
-	/* Register slash command here in on_ready */
+	// Register slash command here in on_ready 
 	bot.on_ready([&bot](const dpp::ready_t& event) {
-		/* Wrap command registration in run_once to make sure it doesnt run on every full reconnection */
+		// Wrap command registration in run_once to make sure it doesnt run on every full reconnection 
 		if (dpp::run_once<struct register_bot_commands>()) {
 			std::vector<dpp::slashcommand> commands {
 				{ "ping", "Ping pong!", bot.me.id }
@@ -30,7 +23,29 @@ int main()
 		}
 	});
 
-	/* Handle slash command with the most recent addition to D++ features, coroutines! */
+	bot.on_guild_member_add([&bot](const dpp::guild_member_add_t& event) {
+		
+		// Channel ID for welcome message to be displayed in
+		dpp::snowflake welcome_channel_id = ;
+
+		// Create the welcome message
+		std::string welcome_message = "Welcome to the server, " + event.added.get_user()->format_username() + "! Nice to see you :)";
+
+		// Send the welcome message to the specified channel
+		bot.message_create(dpp::message(welcome_channel_id, welcome_message));
+
+	});
+
+	bot.on_message_create([&bot](const dpp::message_create_t& event) {
+
+		// Simple testing to initially see if bot worked
+		if (event.msg.content == "testing") {
+			bot.message_create(dpp::message(event.msg.channel_id, "1, 2!"));
+		}
+
+	});
+
+	// Handle slash command with the most recent addition to D++ features, coroutines! 
 	bot.on_slashcommand([](const dpp::slashcommand_t& event) -> dpp::task<void> {
 		if (event.command.get_command_name() == "ping") {
 			co_await event.co_reply("Pong!");
@@ -38,7 +53,7 @@ int main()
 		co_return;
 	});
 
-	/* Start the bot */
+	// Start the bot 
 	bot.start(dpp::st_wait);
 
 	return 0;
